71fd439347ae33d7666e9403d3ec65695df589dd
Jamel EliYah <jamel@ishare.solutions>
2023-09-26 12:35:01 -0400

    saving

---
+++ hamming.py
@@ -0,0 +1,9 @@
+def HammingDistance(strArr):
+
+  # code goes here
+  hammingDistance = HammingDistance(strArr)
+  return strArr
+
+
+# keep this function call here 
+print(HammingDistance(input()))
